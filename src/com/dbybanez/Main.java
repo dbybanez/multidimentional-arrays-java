b9c5bc8989b005dfa78d1d89739b5e6c80ab0d94
dbybanez <dbybanez@gmail.com>
2020-10-19 22:19:05 +0800

    initial commit

---
+++ src/com/dbybanez/Main.java
@@ -0,0 +1,26 @@
+package com.dbybanez;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final byte ROWS = 10;
+        final byte COLS = 8;
+        final int MAX_NUMBER = 99;
+
+        int maxRow = 0;
+        byte indexOfMaxRow = 0;
+
+        Random random = new Random();
+        random.nextInt(MAX_NUMBER);
+
+        int[][] table = new int[ROWS][COLS];
+
+        for (byte rowIndex = 0; rowIndex < ROWS; rowIndex++) {
+            for (byte colIndex = 0; colIndex < COLS; colIndex++) {
+
+            }
+        }
+    }
+}
